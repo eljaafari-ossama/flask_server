6ea305d8ba4647c6a6ef12d5e19a8c638b216b71
eljaafari-ossama <eljaafarioussama9@gmail.com>
2024-07-27 00:38:45 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,48 +1,47 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import mysql.connector
 
 app = Flask(__name__)
 
 # Configuration de la base de données MySQL
 DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '1234',
-    'database': 'report_database'
+    'host': 'localhost',      # Remplacez par votre hôte MySQL
+    'user': 'root',  # Remplacez par votre utilisateur MySQL
+    'password': '1234',  # Remplacez par votre mot de passe MySQL
+    'database': 'report_database'  # Remplacez par le nom de votre base de données
 }
 
-# Fonction pour se connecter à la base de données MySQL
+# Connexion à la base de données
 def get_db_connection():
     conn = mysql.connector.connect(**DB_CONFIG)
     return conn
 
-# Route pour récupérer les données les plus récentes
-@app.route('/latest', methods=['GET'])
+@app.route('/', methods=['GET'])
+def welcome():
+    return 'welcome'
+@app.route('/data/latest', methods=['GET'])
 def get_latest_data():
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
     cursor.execute("SELECT * FROM report_table ORDER BY date_temps DESC LIMIT 1")
-    latest_data = cursor.fetchone()
+    data = cursor.fetchone()
+    cursor.close()
     conn.close()
-    
-    if latest_data:
-        return jsonify(latest_data)
-    else:
-        return jsonify({"error": "No data found"}), 404
+    return jsonify(data)
 
-# Route pour récupérer les données historiques
-@app.route('/historical', methods=['GET'])
+@app.route('/data/historical', methods=['GET'])
 def get_historical_data():
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM report_table ORDER BY date_temps DESC")
-    historical_data = cursor.fetchall()
+    query = "SELECT * FROM report_table WHERE date_temps BETWEEN %s AND %s"
+    cursor.execute(query, (start_date, end_date))
+    data = cursor.fetchall()
+    cursor.close()
     conn.close()
-    
-    if historical_data:
-        return jsonify(historical_data)
-    else:
-        return jsonify([]), 404
+    return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
